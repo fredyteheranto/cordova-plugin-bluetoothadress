763114de2bfce306a79facad266dd0fae71f7f87
fredy saul teheran tovar <fredyteheran91@gmail.com>
2020-05-05 22:39:47 -0500

    fix

--- src/android/com/fredyteheranto/BluetoothAdress.java
+++ src/android/com/fredyteheranto/BluetoothAdress.java
@@ -28,24 +28,13 @@ public class BluetoothAdress extends CordovaPlugin {
   public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
     if (action.equals("bluetoothAdress")) {
 
+      public static final String SECURE_SETTINGS_BLUETOOTH_ADDRESS = "bluetooth_address";
+
+      String macAddress = Settings.Secure.getString(getContentResolver(), SECURE_SETTINGS_BLUETOOTH_ADDRESS);
       // An example of returning data back to the web layer
-      // String phrase = args.getString(0);
       // Echo back the first argument
-      // final PluginResult result = new PluginResult(PluginResult.Status.OK, "Hola
-      // todo el... " + phrase);
-      final BluetoothAdapter bluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
-      String bluetoothMacAddress = "";
-
-      Field mServiceField = bluetoothAdapter.getClass().getDeclaredField("mService");
-      mServiceField.setAccessible(true);
-
-      Object btManagerService = mServiceField.get(bluetoothAdapter);
-
-      if (btManagerService != null) {
-        bluetoothMacAddress = (String) btManagerService.getClass().getMethod("getAddress").invoke(btManagerService);
-        callbackContext.sendPluginResult(bluetoothMacAddress);
-      }
-
+      final PluginResult result = new PluginResult(PluginResult.Status.OK, macAddress);
+      callbackContext.sendPluginResult(result);
     }
     return true;
   }
