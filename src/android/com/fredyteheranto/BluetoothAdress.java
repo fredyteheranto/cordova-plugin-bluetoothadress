215468eea485db3fca2fc2f172506ea22db89d69
fredy saul teheran tovar <fredyteheran91@gmail.com>
2020-05-05 23:14:16 -0500

    fix

--- src/android/com/fredyteheranto/BluetoothAdress.java
+++ src/android/com/fredyteheranto/BluetoothAdress.java
@@ -13,11 +13,13 @@ import android.util.Log;
 import android.bluetooth.BluetoothAdapter;
 import android.os.Bundle;
 import android.content.Intent;
+import android.content.Context
 
 import java.util.Date;
 
 public class BluetoothAdress extends CordovaPlugin {
   private static final String TAG = "BluetoothAdress";
+  private Context context;
 
   public void initialize(CordovaInterface cordova, CordovaWebView webView) {
     super.initialize(cordova, webView);
@@ -25,11 +27,12 @@ public class BluetoothAdress extends CordovaPlugin {
     Log.d(TAG, "Inicializando BluetoothAdress");
   }
 
-  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext) throws JSONException {
+  public boolean execute(String action, JSONArray args, final CallbackContext callbackContext, Context context)
+      throws JSONException {
     if (action.equals("bluetoothAdress")) {
       String SECURE_SETTINGS_BLUETOOTH_ADDRESS = "bluetooth_address";
 
-      String macAddress = android.provider.Settings.Secure.getString(getContentResolver(),
+      String macAddress = android.provider.Settings.Secure.getString(context.getContentResolver(),
           SECURE_SETTINGS_BLUETOOTH_ADDRESS);
       // An example of returning data back to the web layer
       // Echo back the first argument
